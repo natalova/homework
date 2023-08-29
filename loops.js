5a03c98fa3de29461c3f0d483c9df9f4417b9223
natalova <48879751+natalova@users.noreply.github.com>
2023-08-29 21:23:35 +0300

    add_for_in

--- loops.js
+++ loops.js
@@ -17,7 +17,7 @@ let num = 10
 // } while (num <=7)
 
 
-let sumOLoop = 0
+let sumOfLoop = 0
 
 // for (let i = 0; i <= num; i+=2) {
 //     console.log(i)
@@ -30,6 +30,23 @@ for (let i = 0; i <= num; i+=2) {
     
     if (i == 6) continue
     console.log(i)
-    sumOLoop += i
+    sumOfLoop += i
 }
-console.log(sumOLoop)
+console.log(sumOfLoop)
+
+//for in //objects
+//for of //arays
+
+const obj1 = { prop1:1, prop:2, prop3: 'value3'}
+for (let prop in obj1) {
+    console.log(prop)
+    console.log(`${obj1[prop]}`)
+    if (`${obj1[prop]}` == 'New'){
+        console.log
+    }
+}
+
+const arr1 = ['valarr1', 'valarr2', 'valarr3']
+for (let valOfArr of arr1) {
+    console.log(valOfArr)
+}
