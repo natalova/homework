a1d92b0243d052f07c6c344768011a9f3e2bf66d
natalova <48879751+natalova@users.noreply.github.com>
2023-08-29 20:03:37 +0300

    added_else_clause

--- if_else.js
+++ if_else.js
@@ -2,5 +2,18 @@ let num215 = 215
 
 if (num215 == 215) {
     console.log('yes. value under condition')
+    console.log('1')
+    console.log('2')
 }
 
+if (num215 >= 215 && num215 < 250) {
+    console.log('second condition work')
+}
+
+//
+
+if (num215 > 300) {
+    console.log ('num215 variable greater then 300')
+} else {
+console.log ('code goes to the else')
+}
