390e89792351d9dffb88f000f2d929d63f26a25d
natalova <48879751+natalova@users.noreply.github.com>
2023-08-29 20:45:26 +0300

    added_while_loop

--- loops.js
+++ loops.js
@@ -1,2 +1,17 @@
 // the initial changes for work with JS
 // author Nataly K
+
+
+let num = 10
+
+// while (num <= 7) {
+//     console.log (num)
+//     num ++
+// }
+
+// do while
+do {
+    
+    num++
+    console.log (num)
+} while (num <=7)
