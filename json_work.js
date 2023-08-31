53947d3384ca1de764dfc605c711c4082626484d
natalova <48879751+natalova@users.noreply.github.com>
2023-08-31 19:50:34 +0300

    added_changes_before_npm_inslall

--- json_work.js
+++ json_work.js
@@ -1,18 +1,25 @@
+const values = require('./values.json')
+console.log(values)
+
+
+
 // let obj1 = {name: "Natalia", lname:"Kovaleva", age:18}
 // console.log(obj1)
 
 // let jsonObj1 = JSON.stringify(obj1)
 // console.log(jsonObj1)
 
-let jsonObj1 = '{"name": "Natalia", "lname":"Kovaleva", "age":18}'
+////
 
-let obj2 = JSON.parse(jsonObj1)
+// let jsonObj1 = '{"name": "Natalia", "lname":"Kovaleva", "age":18}'
 
-console.log(obj2)
-console.log(obj2.name)
+// let obj2 = JSON.parse(jsonObj1)
 
-obj2.city = "Kharkiv"
-console.log(obj2)
+// console.log(obj2)
+// console.log(obj2.name)
 
-jsonObj1 = JSON.stringify(obj2)
-console.log(jsonObj1)
+// obj2.city = "Kharkiv"
+// console.log(obj2)
+
+// jsonObj1 = JSON.stringify(obj2)
+// console.log(jsonObj1)
