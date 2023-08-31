e6c5c46dc63313022c457b22a926c125a59cd3db
natalova <48879751+natalova@users.noreply.github.com>
2023-08-31 19:47:24 +0300

    add_json_file

---
+++ json_work.js
@@ -0,0 +1,18 @@
+// let obj1 = {name: "Natalia", lname:"Kovaleva", age:18}
+// console.log(obj1)
+
+// let jsonObj1 = JSON.stringify(obj1)
+// console.log(jsonObj1)
+
+let jsonObj1 = '{"name": "Natalia", "lname":"Kovaleva", "age":18}'
+
+let obj2 = JSON.parse(jsonObj1)
+
+console.log(obj2)
+console.log(obj2.name)
+
+obj2.city = "Kharkiv"
+console.log(obj2)
+
+jsonObj1 = JSON.stringify(obj2)
+console.log(jsonObj1)
